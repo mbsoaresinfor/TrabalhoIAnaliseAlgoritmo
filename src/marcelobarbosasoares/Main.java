66068eb2a6fb55b45f6fab69230a5d582f215eff
Marcelo Barbosa Soares <marcelo.soares@cwi.com.br>
2020-09-13 16:15:16 -0300

    remocao dir bin

--- src/marcelobarbosasoares/Main.java
+++ src/marcelobarbosasoares/Main.java
@@ -1,8 +1,5 @@
 package marcelobarbosasoares;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
 
 /**
  * Trabalho da disciplina de Analise de Algoritmo
@@ -12,20 +9,23 @@ import java.util.Set;
 public class Main {
 
 	public static void main(String[] args) {		
-		int[] n = criaN(10);		
-		System.out.println(buscaChave(n,1));
+		int[] n = criaN(100000000);	
+		long tempoInicio = System.currentTimeMillis();
+		System.out.println(buscaChave(n,100000000+1));
+		System.out.println("Tempo Total: "+(System.currentTimeMillis()-tempoInicio));
 	}
 	
 	private static int buscaChave(int[] vetor, int chave) {
-		int chaveEncontrada = -1;					
+		int chaveEncontrada = -1;							
 		for(int i=0; i < vetor.length  ; i++ ) {	
 			if(vetor[i] == chave) {					
-				chaveEncontrada = i;				 
-				break;								
+				chaveEncontrada = i;
+				break;				
 			}
 		}		
-		return chaveEncontrada;						
-	}	
+		return chaveEncontrada;		
+	}								
+	
 	
 	private static int[] criaN(int tamanho) {
 		int[] vetor = new int[tamanho];
